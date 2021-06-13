22e3539724a2baedac892e98d1196eecd8944afd
Neurokid22 <qrankin@icloud.com>
2021-06-13 22:48:32 +0100

    minor adjusts to the app

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ monkey.patch_all()
 from flask import Flask, request, jsonify, render_template
 from flask_socketio import SocketIO, send, emit
 from threading import Lock
-from dotenv import load_dotenv
 from binance_streamer import BinanceStreamer
 from datetime import datetime
 from os.path import exists
@@ -17,15 +16,9 @@ app = Flask(__name__)
 socketio = SocketIO(app, async_mode="gevent")
 
 # Credentials
-# load_dotenv(".env")
 API_KEY = os.environ["API_KEY"]
 SECRET_KEY = os.environ["SECRET_KEY"]
 app.config['SECRET_KEY'] = os.environ["SECRET"]
-
-# API_KEY = os.getenv("API_KEY")
-# SECRET_KEY = os.getenv("SECRET_KEY")
-# app.config['SECRET_KEY'] = os.getenv("SECRET")
-
 
 
 thread_lock = Lock()
